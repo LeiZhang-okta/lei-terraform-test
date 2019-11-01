e37809d64e2d54f01bad09da5127af09abed244f
LeiZhang-okta <55606485+LeiZhang-okta@users.noreply.github.com>
2019-11-01 21:12:42 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 provider "okta" {
-  org_name  = <okta instance name, e.g. dev-XXXXXX>
-  api_token = <okta instance api token with the Administrator role>
-  base_url  = <okta base url, e.g. oktapreview.com>
-
-  // Optional settings, https://en.wikipedia.org/wiki/Exponential_backoff
-  max_retries      = <number of retries on api calls, default: 5>
-  backoff          = <enable exponential backoff strategy for rate limits, default = true>
-  min_wait_seconds = <min number of seconds to wait on backoff, default: 30>
-  max_wait_seconds = <max number of seconds to wait on backoff, default: 300>
+  org_name  = dev-123
+  api_token = xxx
+  base_url  = oktapreview.com
 }
