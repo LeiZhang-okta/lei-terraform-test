ad04612cc81c5dc010cd2fdd59c334d300b40b12
Lei Zhang <lei.zhang@okta.com>
2019-11-01 21:27:03 +1100

    init

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "okta" {
   org_name  = dev-835967
-  api_token = 00e34pDbneB8n6afos31zs-Mp39BXnwPo4hsj9-09v
-  
+  api_token = 00e34pDbneB8n6afos31zs-Mp39BXnwPo4hsj9-09v  
   base_url  = oktapreview.com
 }
