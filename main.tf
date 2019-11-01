b48c12ef7206778c4037907aa5c19c2c9f54a3b1
LeiZhang-okta <55606485+LeiZhang-okta@users.noreply.github.com>
2019-11-01 21:11:13 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-provider "aws" {
-  profile    = "aue1t"
-  region     = "us-east-1"
+provider "okta" {
+  org_name  = <okta instance name, e.g. dev-XXXXXX>
+  api_token = <okta instance api token with the Administrator role>
+  base_url  = <okta base url, e.g. oktapreview.com>
+
+  // Optional settings, https://en.wikipedia.org/wiki/Exponential_backoff
+  max_retries      = <number of retries on api calls, default: 5>
+  backoff          = <enable exponential backoff strategy for rate limits, default = true>
+  min_wait_seconds = <min number of seconds to wait on backoff, default: 30>
+  max_wait_seconds = <max number of seconds to wait on backoff, default: 300>
 }
