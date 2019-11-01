bbf3530684303b7ab56801d957dda11b06fb2c6e
Lei Zhang <lei.zhang@okta.com>
2019-11-01 21:57:15 +1100

    change to okta

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-provider "github" {
-  org_name  = "dev-835967"
-  api_token = "00ldO5Lkdm1mq-pL_4xqTCmZDgoHKn0RPBXF1Fmdqt"
-  base_url  = "oktapreview.com"
+provider "okta" {
+  #org_name  = "dev-835967"
+  #api_token = "00ldO5Lkdm1mq-pL_4xqTCmZDgoHKn0RPBXF1Fmdqt"
+  #base_url  = "oktapreview.com"
 }
